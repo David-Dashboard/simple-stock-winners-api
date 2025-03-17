c47e898c81877e7a7643b5fc361091cfcfdc7fc1
Fu_David <David.Bao.Fu@sony.com>
2025-03-17 19:15:51 +0100

    [update] First REST service

--- README.md
+++ README.md
@@ -6,6 +6,12 @@ Simplicity:
 - `fastapi` for lightweight REST API
 - `pytest` for test-driven development
 
+Instructions:
+- `fastapi dev main.py`
+
+About `requirements.txt`:
+- Generated using `pipreqs`
+
 TODO (2025-03-15):
 - Write tests using pytest
 - Figure out a good directory structure for the project (where to put sample data, tests and source code) 
--- data.py
+++ data.py
@@ -1,15 +1,59 @@
 import json
+import numpy as np
 import pandas as pd
+from datetime import date, timedelta
 
 def get_stock_winners():
 
     df = get_sample_indata ()
+
+    # Format all data correct dtypes
+    df["Date"] = pd.to_datetime(df["Date"])
+    #assert df["Kurs"].dtype == np.int64
+
+    # Set up computation
+    company_names = df["Kod"].unique().tolist() # np.ndarray -> list
+
+    df["Day"] = df["Date"].apply(lambda x: x.date()) # Grab date without time
+    
+    latest_kurs_by_day = df.groupby(["Kod", "Day"]).apply(lambda x: x.loc[x["Date"].idxmax(), "Kurs"], include_groups=False) # Grab latest kurs by day
+    data_med_kursavslut = latest_kurs_by_day.reset_index(name='Kursavslut')
+
+    # Antaganden: Finns inga dagar som saknas och de är sorterade
+    # Räkna ut kursskillnad, saknas data från föregående dag 
+    kursskillnad = data_med_kursavslut.groupby("Kod").apply(lambda x: x["Kursavslut"].diff())
+    data_med_kursavslut["Kursskillnad"] = kursskillnad.reset_index(name="Kursskillnad")["Kursskillnad"]
+
+    #100 * diff / (curr + diff)
+    data_med_kursavslut["Kursskillnad (procent)"] = 100 * data_med_kursavslut["Kursskillnad"] / (data_med_kursavslut["Kursskillnad"] + data_med_kursavslut["Kursavslut"])
     
 
-    return get_sample_utdata ()
+    latest_timestamp = df["Date"].max().date()
+    sorterad_data = data_med_kursavslut[data_med_kursavslut["Day"] == latest_timestamp].sort_values(by="Kursskillnad (procent)", ascending=False)
+    
+    sorterad_data = sorterad_data.reset_index(drop=True)
+    sorterad_data = sorterad_data.rename(columns={
+        "Kod": "name",
+        "Kursskillnad (procent)": "percent",
+        "Kursavslut": "latest"
+        })
+    sorterad_data["rank"] = sorterad_data.index + 1
+
+    
+    utdata = {
+            "winners": json.loads(sorterad_data[["rank", "name", "percent", "latest"]].iloc[:3].to_json(orient="records", double_precision=2))
+        }
+    
+
+
+    # Missing Kurs for today
+    # Missing Kurs for yesterday
+    # Missing company for yesterday
+    # Missing company for today
+
+    return utdata
 
 def get_sample_indata ():
-    print(df)
     return pd.read_csv ("sample_data/sample_indata.csv")
 
 def get_sample_utdata ():
@@ -21,4 +65,6 @@ def get_sample_utdata ():
 
 
 if __name__ == "__main__":
-    print(get_stock_winners())
+    print("\nSample indata:\n", get_sample_indata())
+    print("\nSample utdata:\n", get_sample_utdata())
+    print("\nSample utdata after computation using indata:\n", get_stock_winners())
